b6dfb77e62183eb409f961733fa77591d741c88c
ArmanAru <armanhosen768@gmail.com>
2022-11-09 12:54:44 +0600

    env

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ console.log(process.env.DB_PASSWORD)
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yxbgeqc.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
