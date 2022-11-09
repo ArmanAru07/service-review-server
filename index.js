6451357a4229f68d1aa904dd6eb58f511692c376
ArmanAru <armanhosen768@gmail.com>
2022-11-09 12:51:44 +0600

    mongoDBconnection

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -7,10 +10,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('genius csr server is running')
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yxbgeqc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('service review server is running')
 })
 
-app.listen(post, () =>{
-    console.log(`Genius car server running on ${port}`);
+app.listen(port, () => {
+    console.log(`service review server running on ${port}`);
 })
